aac52779ebb13102c22bb75ce71593f0a7c6d320
KaFelLa92 <choidj10@gmail.com>
2025-08-03 23:16:54 +0900

    실습2 진행중

---
+++ src/main/java/example/day03/실습/BoardControl.java
@@ -0,0 +1,18 @@
+package example.day03.실습;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class BoardControl {
+
+    // 1. post 매핑
+    // URL /board
+    // 내용과 작성자 입력받아 저장
+    // String bcontent , bwriter BoardDto
+    @PostMapping("/board")
+    @ResponseBody
+    public boolean boardWrite(){
+        System.out.println("BoardControl.boardWrite");
+        return false;
+    }
+}
---
+++ src/main/java/example/day03/실습/BoardDto.java
@@ -0,0 +1,68 @@
+package example.day03.실습;
+
+public class BoardDto {
+    // 1. 멤버변수
+    private int bno;
+    private String bcontent;
+    private String bwriter;
+
+    // 2. 생성자
+    // 2-1) 전체
+    public BoardDto(int bno, String bcontent, String bwriter) {
+        this.bno = bno;
+        this.bcontent = bcontent;
+        this.bwriter = bwriter;
+    }
+
+    // 2-2) Post용
+    public BoardDto(String bcontent, String bwriter) {
+        this.bcontent = bcontent;
+        this.bwriter = bwriter;
+    }
+
+    // 2-3) 개별조회, 삭제용
+    public BoardDto(int bno) {
+        this.bno = bno;
+    }
+
+    // 2-4) 수정용
+    public BoardDto(int bno, String bcontent) {
+        this.bno = bno;
+        this.bcontent = bcontent;
+    }
+
+
+    // 3. 메소드 (getter and setter , toString)
+    public int getBno() {
+        return bno;
+    }
+
+    public void setBno(int bno) {
+        this.bno = bno;
+    }
+
+    public String getBcontent() {
+        return bcontent;
+    }
+
+    public void setBcontent(String bcontent) {
+        this.bcontent = bcontent;
+    }
+
+    public String getBwriter() {
+        return bwriter;
+    }
+
+    public void setBwriter(String bwriter) {
+        this.bwriter = bwriter;
+    }
+
+    @Override
+    public String toString() {
+        return "BoardDto{" +
+                "bno=" + bno +
+                ", bcontent='" + bcontent + '\'' +
+                ", bwriter='" + bwriter + '\'' +
+                '}';
+    }
+}
